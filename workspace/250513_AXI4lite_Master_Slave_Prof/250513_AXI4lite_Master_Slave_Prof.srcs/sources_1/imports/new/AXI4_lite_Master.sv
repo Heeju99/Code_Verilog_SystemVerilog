c55934406f5982a1d1b4b381cda81d59182d59be
Heeju99 <134408875+Heeju99@users.noreply.github.com>
2025-05-13 12:54:01 +0900

    Auto commit for modified projects on 2025-05-13 12:54

---
+++ workspace/250513_AXI4lite_Master_Slave_Prof/250513_AXI4lite_Master_Slave_Prof.srcs/sources_1/imports/new/AXI4_lite_Master.sv
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+module AXI_lite_Master(
+    //Global Signal
+    input logic ACLK,
+    input logic ARESETn
+);  
+endmodule
---
+++ workspace/250513_AXI4lite_Master_Slave_Prof/250513_AXI4lite_Master_Slave_Prof.srcs/sources_1/imports/new/AXI4_lite_Slave.sv
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+module AXI4_lite_Slave(
+    //Global Signal
+    input logic ACLK,
+    input logic ARESETn,
+);
+endmodule
